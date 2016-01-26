169ab81423800e4cb578fdde8f13bf62438f427e
Kazuyuki Matsuo <matu.iphone@gmail.com>
2016-01-26 11:27:08 +0900

    revert test util fils

---
+++ test/testUtil/commentUtil.js
@@ -0,0 +1,15 @@
+export function getPeteComment() {
+  return {
+      "id": 1388534400000,
+      "author": "Pete Hunt",
+      "text": "Hey there!"
+  }
+}
+
+export function getPaulComment() {
+  return {
+      "id": 1420070400000,
+      "author": "Paul O’Shannessy",
+      "text": "React is *great*!"
+  }
+}
